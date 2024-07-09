295e145fb2205367b142061e73d9d9398bb08f05
Mrjeerryy <145805657+Mrjeerryy@users.noreply.github.com>
2024-07-09 15:32:14 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:news/screens/main/main_screen.dart';
+
+import 'constants.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: kPrimaryColor,
+        scaffoldBackgroundColor: kBgColor,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: TextButton.styleFrom(backgroundColor: kPrimaryColor),
+        ),
+        textTheme: TextTheme(
+          bodySmall: TextStyle(color: kBodyTextColor),
+          bodyMedium: TextStyle(color: kBodyTextColor),
+          headlineLarge: TextStyle(color: kDarkBlackColor),
+        ),
+      ),
+      home: MainScreen(),
+    );
+  }
+}
